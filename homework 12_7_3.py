7fba055b96ad29e80d4a3e8c25ce9ed4d1a8b903
EkaterinaChechik <andreeva.chechik@gmail.com>
2022-08-31 21:53:33 +0200

    изменено задание модуля 12

--- homework 12_7_3.py
+++ homework 12_7_3.py
@@ -1,6 +1,9 @@
 # задание 12.7.3
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+print( per_cent )
+
 money = input('Введите денежную сумму ')
 deposit = list( map( lambda x : int(float(money)*x), per_cent.values() ) )
+
 print ("Возможная прибыль в банках", deposit)
 print(" Максимальная сумма, которую вы можете заработать — ", max(deposit) )  
